bb10394575a4bd92bf754a2c201f5b879067fc51
Vadim <kokorev.vadim.123456789@gmail.com>
2024-11-23 19:15:50 +0500

    -Updates

--- Bot/misc/bot.py
+++ Bot/misc/bot.py
@@ -7,14 +7,11 @@ BOT_TOKEN = config.tg_bot.token
 
 bot = Bot(BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
 
-client = ""
-assistant = ""
+client = OpenAI(api_key=f"{config.gpt.api_key}")
 
-# client = OpenAI(api_key=f"{config.gpt.api_key}")
-#
-# assistant = client.beta.assistants.create(
-#     name="SMM Helper",
-#     instructions="Ты СММ помощник. Твоя задача помогать людям с их постами для соц. сетей. Всегда отвечай людям на русском языке. Всегда проверяй себя на орфографию и другие ошибки в предложениях",
-#     tools=[{"type": "code_interpreter"}],
-#     model="gpt-4o",
-# )
+assistant = client.beta.assistants.create(
+    name="SMM Helper",
+    instructions="Ты СММ помощник. Твоя задача помогать людям с их постами для соц. сетей. Всегда отвечай людям на русском языке. Всегда проверяй себя на орфографию и другие ошибки в предложениях",
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4o",
+)
