bf4cdb7b82e51396513c7295b7a700d9c1649515
Vadim <kokorev.vadim.123456789@gmail.com>
2025-02-23 22:59:59 +0500

    -website design

--- Backend.py
+++ Backend.py
@@ -245,7 +245,7 @@ async def get_confirmation_token(payment_request: PaymentRequest):
 
 app.include_router(mainpage_router)
 if __name__ == "__main__":
-    try:
-        uvicorn.run(app, host="0.0.0.0", port=443, ssl_keyfile="privkey.pem", ssl_certfile="fullchain.pem")
-    except:
-        uvicorn.run(app, host="127.0.0.1", port=80)
+
+    uvicorn.run(app, host="0.0.0.0", port=443, ssl_keyfile="privkey.pem", ssl_certfile="fullchain.pem")
+    # except:
+    #     uvicorn.run(app, host="127.0.0.1", port=80)
