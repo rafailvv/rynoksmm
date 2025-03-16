d224c85b93e92985c70735a5032adf9622555024
Vadim <kokorev.vadim.123456789@gmail.com>
2025-03-16 17:43:21 +0500

    -website design

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 
-from aiogram.fsm.storage import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.storage.redis import RedisStorage, Redis
 from aiogram import Bot, Dispatcher
 
